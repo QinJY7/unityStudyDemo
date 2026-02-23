37f345d3bf33abf04ca13d2511a97b4c2fe550ff
QinJY7 <1711952836@qq.com>
2026-02-23 18:22:57 +0800

    创建项目，第一个输出

---
+++ Assets/Scripts/debugScripts.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class debugScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("蛊真人，第一个脚本运行了");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
